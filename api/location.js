28ab182d261bde85c428424b0318c3a7e8f6a703
Francisco Heronildo <heronildo.2010@hotmail.com>
2024-09-20 10:55:06 -0300

    update

--- api/location.js
+++ api/location.js
@@ -4,16 +4,18 @@ module.exports = async (req, res) => {
     try {
         const { lat, lon } = req.query;
 
+        console.log("Received coordinates:", lat, lon);
+
         if (!lat || !lon) {
             return res.status(400).json({ error: 'Latitude e longitude são obrigatórias' });
         }
-
-        // Fazendo a requisição à API Nominatim com um User-Agent customizado
         const response = await axios.get(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`, {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (compatible; Vercel/1.0)'
             }
         });
+
+        console.log("Nominatim API response:", response.data);
 
         if (response.data && response.data.address) {
             const address = response.data.address;
@@ -26,6 +28,7 @@ module.exports = async (req, res) => {
             res.status(404).json({ error: 'Endereço não encontrado' });
         }
     } catch (error) {
+        console.error("Error fetching address:", error.message);
         res.status(500).json({ error: 'Erro ao buscar o endereço' });
     }
 };
